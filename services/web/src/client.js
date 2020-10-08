2a236b505778658f689073412fa20129fbfb988c
letoast <ciroman@gmail.com>
2020-10-08 13:30:51 +0000

    added initFeatureToggles to src/client.js

--- services/web/src/client.js
+++ services/web/src/client.js
@@ -1,4 +1,7 @@
 import * as sapper from '@sapper/app';
+import { initFeatureToggles } from './feature-toggles/index.js'
+
+initFeatureToggles()
 
 sapper.start({
 	target: document.querySelector('#sapper')
