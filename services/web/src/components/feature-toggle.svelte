0ab5e6cbe9beac1e339badbd20734249ac4ab151
letoast <ciroman@gmail.com>
2020-10-09 10:53:33 +0000

    Finished adding the firebase remote config component and setup

---
+++ services/web/src/components/feature-toggle.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { featureToggleStore } from '../feature-toggles/store'
+    export let name
+</script>
+
+{#if $featureToggleStore[name]}
+    <slot />
+{/if}
--- services/web/src/feature-toggles/index.js
+++ services/web/src/feature-toggles/index.js
@@ -1,11 +1,25 @@
+import { featureToggleStore } from './store.js'
+
 export const FEATURE_TOGGLES = {
- vipWelcome: {
- name: "feature_vip_welcome"
- }
+    vipWelcome: {
+    name: "feature_vip_welcome"
+    }
 }
 
+const setFeatureTogglesStore = (configValues) =>
+    Object.keys(FEATURE_TOGGLES).forEach( toggleKey => {
+        const featureToggleName = FEATURE_TOGGLES[toggleKey].name
+        if( configValues[featureToggleName] ) {
+            featureToggleStore.setFeatureToggle(
+                featureToggleName,
+                configValues[featureToggleName].asBoolean()
+            )
+        }
+    })
+
 export const initFeatureToggles = async () => {
- const remoteConfig = window.firebase.remoteConfig();
- await remoteConfig.fetchAndActivate();
- return remoteConfig;
+    const remoteConfig = window.firebase.remoteConfig();
+    await remoteConfig.fetchAndActivate();
+    setFeatureTogglesStore(remoteConfig.getAll())
+    return remoteConfig;
 };
